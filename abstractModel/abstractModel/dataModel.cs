90cc96591f5a0f6420c2d886156b09245cfe1abd
jasonnguyenvn <conghauit@outlook.com>
2015-08-04 22:46:08 +0700

    commit the full abstract class of dataModel

--- abstractModel/abstractModel/dataModel.cs
+++ abstractModel/abstractModel/dataModel.cs
@@ -11,11 +11,13 @@ namespace abstractModel
     public class dataModel<T>
     {
         private List<T> _data;
-        protected string _host;
-        protected int _port;
-        protected string _dbname;
-        protected string _username;
-        protected string _password;
+        private string _host;
+        private int _port;
+        private string _dbname;
+        private string _username;
+        private string _password;
+
+        protected SqlConnection conn;
 
         private readonly string _tbname;
 
@@ -36,34 +38,78 @@ namespace abstractModel
             this._tbname = table_name;
 
             this._data = new List<T>();
+            this.conn = this.createConnect();
         }
 
 
-        protected SqlConnection connect()
+        protected SqlConnection createConnect()
         {
             SqlConnection conn = new SqlConnection();
             conn.ConnectionString = @"server="+this._host
                                     +";database="+this._dbname
                                     +";User ID="+this._username
                                     +";Password="+this._password+";";
-            conn.Open();
             return conn;
         }
 
 
-        private SqlParameter createSQLParam(string key, SqlDbType dbType, object value, int size)
+        protected SqlParameter createSQLParam(string key, SqlDbType dbType, object value, int size)
         {
             SqlParameter param = new SqlParameter(key, dbType, size);
             param.Value = value;
             return param;
         }
 
-        private SqlParameter createSQLParam(string key, SqlDbType dbType, object value)
+        protected SqlParameter createSQLParam(string key, SqlDbType dbType, object value)
         {
             SqlParameter param = new SqlParameter(key, dbType);
             param.Value = value;
             return param;
         }
+
+        protected SqlCommand createSQLCommand(string commandText, CommandType commandType, List<SqlParameter> parameters)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = this.conn;
+            cmd.CommandText = commandText;
+            cmd.CommandType = commandType;
+
+            if(parameters!=null)
+            {
+                foreach (SqlParameter param in parameters)
+                {
+                    cmd.Parameters.Add(param);
+                }
+            }
+
+            return cmd;
+        }
+
+        protected SqlCommand createSQLCommand(string commandText)
+        {
+            return this.createSQLCommand(commandText, CommandType.Text, null);
+        }
+
+
+        protected SqlDataReader getRows(string where_filter)
+        {
+            string command = "SELECT * FROM " + this._tbname;
+            if (where_filter.Equals("") == false)
+                command += " WHERE " + where_filter;
+            this.conn.Open();
+            SqlCommand cmd = this.createSQLCommand(command);
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            dr.Close();
+            this.conn.Close();
+            return dr;
+        }
+
+        protected SqlDataReader getRows()
+        {
+            return this.getRows("");
+        }
+
 
     }
 }
